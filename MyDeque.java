f94e287aa43e786bc829509621f068b955614b94
Julian Wong <jwong01@stuy.edu>
2019-03-25 14:45:12 -0400

    add and remove first work

--- MyDeque.java
+++ MyDeque.java
@@ -33,6 +33,20 @@ public class MyDeque<E> {
     if(size + 1 >= data.length) {
       copyData();
     }
+    if(data[0] == null) {
+      data[0] = e;
+      size++;
+      return;
+    }
+    int qi = 0;
+    while(qi < data.length && data[qi] != null) {
+      qi++;
+    }
+    for(int qw = qi; qw > 0; qw--) {
+      data[qw] = data[qw - 1];
+    }
+    data[0] = e;
+    size++;
   }
   public void addLast(E e) throws NullPointerException {
     if(size + 1 >= data.length) {
@@ -40,10 +54,20 @@ public class MyDeque<E> {
     }
   }
   public E removeFirst() throws NoSuchElementException {
-    return null;
+    E ret = data[0];
+    data[0] = null;
+    return ret;
   }
   public E removeLast() throws NoSuchElementException {
-    return null;
+    E ret = null;
+    for(int q = data.length - 1; q >= 0; q--) {
+      if(data[q] != null) {
+        ret = data[q];
+        data[q] = null;
+        break;
+      }
+    }
+    return ret;
   }
   public E getFirst() throws NoSuchElementException {
     int q = 0;
@@ -66,5 +90,16 @@ public class MyDeque<E> {
       teli[q] = data[q];
     }
     data = teli;
+  }
+  public static void main(String[] cvf) {
+    MyDeque<Integer> fhfh = new MyDeque<Integer>();
+    fhfh.addFirst(1);
+    System.out.println(fhfh);
+    fhfh.addFirst(0);
+    System.out.println(fhfh);
+    fhfh.addFirst(2);
+    System.out.println(fhfh);
+    System.out.println("removed: " + fhfh.removeFirst());
+    System.out.println(fhfh);
   }
 }
