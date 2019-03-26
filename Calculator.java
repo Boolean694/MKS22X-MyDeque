b5fe6224bdbf4be58571fbe08c5de914032c2598
Julian Wong <jwong01@stuy.edu>
2019-03-26 14:46:14 -0400

    add numbers to stack

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,15 @@
 public class Calculator {
   public static double eval(String inp) {
     String[] chli = inp.split(" ");
-    return 0.0;
+    MyDeque<Double> dq = new MyDeque<Double>(chli.length * 2);
+    for(int q = 0; q < chli.length; q++) {
+      try{dq.addLast(Double.parseDouble(chli[q])); chli[q] = "";}
+      catch(NumberFormatException bsdf) {}
+    }
+    System.out.println(dq);
+    return 1;
   }
   public static void main(String[] afaf) {
-
+    eval("4 8 - 6 2 + 7 1 * 5 3 /");
   }
 }
