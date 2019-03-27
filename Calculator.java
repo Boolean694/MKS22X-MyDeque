88f10cf972415c15ef00a0015b4765fd225eeb67
Julian Wong <jwong01@stuy.edu>
2019-03-26 23:39:12 -0400

    stuff

--- Calculator.java
+++ Calculator.java
@@ -6,8 +6,35 @@ public class Calculator {
       try{dq.addLast(Double.parseDouble(chli[q])); chli[q] = "";}
       catch(NumberFormatException bsdf) {}
     }
-    System.out.println(dq);
-    return 1;
+    for(int q = 0; q < chli.length; q++) {
+      if(chli[q] != "") {
+        double f = dq.removeLast();
+        double s = dq.removeLast();
+        double res;
+        if(chli[q] == "+") {
+          res = f + s;
+          dq.addLast(res);
+        }
+        if(chli[q] == "-") {
+          res = f - s;
+          dq.addLast(res);
+        }
+        if(chli[q] == "*") {
+          res = f * s;
+          dq.addLast(res);
+        }
+        if(chli[q] == "/") {
+          res = f / s;
+          dq.addLast(res);
+        }
+        if(chli[q] == "%") {
+          res = f % s;
+          dq.addLast(res);
+        }
+      }
+      System.out.println(dq);
+    }
+    return dq.getLast();
   }
   public static void main(String[] afaf) {
     eval("4 8 - 6 2 + 7 1 * 5 3 /");
