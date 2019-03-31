ccaf5c76a65f2f5cd85070cf5b4a612195c917ae
Julian Wong <jwong01@stuy.edu>
2019-03-31 15:00:34 -0400

    works

--- Calculator.java
+++ Calculator.java
@@ -3,39 +3,39 @@ public class Calculator {
     String[] chli = inp.split(" ");
     MyDeque<Double> dq = new MyDeque<Double>(chli.length * 2);
     for(int q = 0; q < chli.length; q++) {
-      if(chli[q].equals("+") || chli[q].equals("-") || chli[q].equals("*") || chli[q].equals("/")) {
-        Double f = dq.removeLast();
-        Double s = dq.removeLast();
-        Double res;
-        if(chli[q] == "+") {
-          res = f + s;
+      if(chli[q].equals("+") || chli[q].equals("-") || chli[q].equals("*") || chli[q].equals("/") || chli.equals("%")) {
+        double f = dq.removeLast();
+        double s = dq.removeLast();
+        double res;
+        if(chli[q].equals("+")) {
+          res = s + f;
           dq.addLast(res);
         }
-        if(chli[q] == "-") {
-          res = f - s;
+        if(chli[q].equals("-")) {
+          res = s - f;
           dq.addLast(res);
         }
-        if(chli[q] == "*") {
-          res = f * s;
+        if(chli[q].equals("*")) {
+          res = s * f;
           dq.addLast(res);
         }
-        if(chli[q] == "/") {
-          res = f / s;
+        if(chli[q].equals("/")) {
+          res = s / f;
           dq.addLast(res);
         }
-        if(chli[q] == "%") {
-          res = f % s;
+        if(chli[q].equals("%")) {
+          res = s % f;
           dq.addLast(res);
         }
       }
       else {
         dq.addLast(Double.parseDouble(chli[q]));
       }
-      System.out.println(dq);
     }
+    System.out.println(dq.getLast());
     return dq.getLast();
   }
   public static void main(String[] afaf) {
-    eval("4 8 - 6 2 + 7 1 * 5 3 /");
+    eval("3 4 + 2 * 7 /");
   }
 }
